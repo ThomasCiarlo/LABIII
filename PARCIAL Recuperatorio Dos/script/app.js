4dbab6f8334145fe8fc9e016adb0c9e12e35f08b
unknown <thomasciarlo18@gmail.com>
2021-07-30 20:19:43 -0300

    listo

---
+++ PARCIAL Recuperatorio Dos/script/app.js
@@ -0,0 +1,6 @@
+export var Etransaccion;
+(function (Etransaccion) {
+    Etransaccion[Etransaccion["Venta"] = 0] = "Venta";
+    Etransaccion[Etransaccion["Alquiler"] = 1] = "Alquiler";
+    Etransaccion[Etransaccion["Permuta"] = 2] = "Permuta";
+})(Etransaccion || (Etransaccion = {}));
